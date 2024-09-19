862255a7a03b2098833d7c35b3acac8a08bc9be2
Eden <yikzeng@cisco.com>
2024-09-19 17:12:28 +0800

    fix some issues

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,18 +3,9 @@
     <q-page-container>
       <audio id="player">Player</audio>
       <div id="sentences">
-        <q-list
-          class="sentence"
-          v-for="s in player.parsedSentences.value"
-          :key="s"
-          @click="player.clickSentence(s)"
-        >
+        <q-list class="sentence" v-for="s in player.parsedSentences.value" :key="s" @click="player.clickSentence(s)">
           <q-item clickable v-ripple>
-            <q-item-section
-              :id="s.start.toString()"
-              v-if="player.currentSentence.value === s"
-              class="text-primary"
-            >
+            <q-item-section :id="s.start.toString()" v-if="player.currentSentence.value === s" class="text-primary">
               {{ s.text }}
             </q-item-section>
             <q-item-section :id="s.start.toString()" v-else>
@@ -24,82 +15,46 @@
         </q-list>
       </div>
     </q-page-container>
-    <q-drawer
-      show-if-above
-      v-model="rightDrawerOpen"
-      side="right"
-      behavior="desktop"
-      bordered
-    >
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" behavior="desktop" bordered>
       <q-list dense bordered>
-        <q-item
-          clickable
-          v-ripple
-          v-for="lesson in lessons"
-          :key="lesson.name"
-          @click="playLesson(lesson)"
-        >
+        <q-item clickable v-ripple v-for="lesson in lessons" :key="lesson.name" @click="playLesson(lesson)">
           <q-item-section>
             <q-item-label>{{ lesson.name }}</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
-    <q-footer elevated class="bg-grey-8 text-white">
-      <div class="q-pa-md">
-        <div class="row">
-          <q-btn
-            flat
-            class="col"
-            @click="player.repeatOne.value = !player.repeatOne.value"
-            dense
-            :icon="
-              player.repeatOne.value === true ? 'repeat_one_on' : 'repeat_one'
-            "
-          />
+    <q-footer class="bg-grey-8 text-white">
+      <div class="row">
+        <q-btn flat class="col-2" @click="player.repeatOne.value = !player.repeatOne.value" dense :icon="player.repeatOne.value === true ? 'repeat_one_on' : 'repeat_one'
+          " />
 
-          <q-toolbar class="col-8">
-            <q-btn
-              push
-              glossy
-              label="prev"
-              icon="skip_previous"
-              @click="prev"
-            />
-            <q-btn
-              push
-              glossy
-              label="before"
-              icon="navigate_before"
-              @click="beforeSentence"
-            />
+        <q-toolbar class="col-8">
+          <div class="fit row wrap justify-center ">
+            <q-btn flat icon="skip_previous" @click="prev">
+              <q-tooltip>Previous lesson</q-tooltip>
+            </q-btn>
+            <q-btn flat icon="navigate_before" @click="beforeSentence">
+              <q-tooltip>Previous sentence</q-tooltip>
+            </q-btn>
+            <q-btn v-if="player.sound.value && player.sound.value.playing()" flat icon="pause_circle"
+              @click="togglePlay">
+              <q-tooltip>Pause</q-tooltip>
+            </q-btn>
+            <q-btn v-else flat icon="play_circle" @click="togglePlay">
+              <q-tooltip>Play</q-tooltip>
+            </q-btn>
 
-            <q-btn
-              push
-              glossy
-              label="play/pause"
-              icon="play_circle"
-              @click="togglePlay"
-            />
-            <q-btn
-              push
-              glossy
-              label="next"
-              icon="navigate_next"
-              @click="nextSentence"
-            />
+            <q-btn flat icon="navigate_next" @click="nextSentence">
+              <q-tooltip>Next sentence</q-tooltip>
+            </q-btn>
+            <q-btn flat icon="skip_next" @click="next">
+              <q-tooltip>Next lesson</q-tooltip>
+            </q-btn>
+          </div>
+        </q-toolbar>
 
-            <q-btn push glossy label="next" icon="skip_next" @click="next" />
-          </q-toolbar>
-
-          <q-btn
-            flat
-            class="col"
-            @click="toggleRightDrawer"
-            dense
-            icon="reorder"
-          />
-        </div>
+        <q-btn flat class="col-2" @click="toggleRightDrawer" dense icon="reorder" />
       </div>
     </q-footer>
   </q-layout>
@@ -127,7 +82,7 @@ onMounted(() => {
 class Player {
   constructor() {
     this.prevSound = null;
-    this.sound = null;
+    this.sound = ref(null);
     this.currentLesson = null;
     this.currentSentence = ref(null);
     this.parsedSentences = ref([]);
@@ -137,7 +92,7 @@ class Player {
   }
 
   playLesson(lesson) {
-    this.prevSound = this.sound;
+    this.prevSound = this.sound.value;
     this.currentLesson = lesson;
     const lyricsPath = lesson.lyrics.path + "/" + lesson.lyrics.name;
     const audioFilePath = lesson.audio.path + "/" + lesson.audio.name;
@@ -149,28 +104,25 @@ class Player {
         return this.initializeSound(audioFilePath);
       })
       .then(() => {
-        // 音频初始化完成后的操作
-        console.log("课程加载完成");
         if (this.prevSound) {
           this.prevSound.pause();
         }
         this.clickSentence(this.parsedSentences.value[0]);
       })
       .catch((error) => {
-        console.error("加载课程时出错:", error);
-        // 这里可以添加错误处理逻辑，比如显示错误消息给用户
+        console.error("Error:", error);
       });
   }
 
   initializeSound(audioFilePath) {
     return new Promise((resolve, reject) => {
-      this.sound = new Howl({
+      this.sound.value = new Howl({
         src: [audioFilePath],
         html5: true,
         onload: () => {
           this.parsedSentences.value[
             this.parsedSentences.value.length - 1
-          ].end = this.sound.duration();
+          ].end = this.sound.value.duration();
           resolve();
         },
         onloaderror: (id, error) => {
@@ -184,16 +136,15 @@ class Player {
   }
 
   togglePlay() {
-    console.log("togglePlay");
-    if (this.sound.playing()) {
-      this.sound.pause();
+    if (this.sound.value.playing()) {
+      this.sound.value.pause();
     } else {
-      this.sound.play();
+      this.sound.value.play();
     }
   }
 
   step() {
-    if (!this.sound.playing()) {
+    if (!this.sound.value.playing()) {
       return;
     }
 
@@ -202,14 +153,14 @@ class Player {
       return;
     }
 
-    var currentTime = this.sound.seek() || 0;
+    var currentTime = this.sound.value.seek() || 0;
 
     if (
       this.currentSentence.value &&
       currentTime >= this.currentSentence.value.end &&
       this.repeatOne.value
     ) {
-      this.sound.seek(this.currentSentence.value.start);
+      this.sound.value.seek(this.currentSentence.value.start);
       requestAnimationFrame(this.step.bind(this));
       return;
     }
@@ -248,16 +199,16 @@ class Player {
   }
 
   async clickSentence(sentence) {
-    if (!this.sound) return;
+    if (!this.sound.value) return;
     this.stopCurrentPlayback();
     this.currentSentence.value = sentence;
-    this.sound.seek(sentence.start);
-    this.sound.play();
+    this.sound.value.seek(sentence.start);
+    this.sound.value.play();
   }
 
   stopCurrentPlayback() {
-    if (this.sound.playing()) {
-      this.sound.pause();
+    if (this.sound.value.playing()) {
+      this.sound.value.pause();
     }
     if (this.currentPause) {
       clearTimeout(this.currentPause);
@@ -336,8 +287,8 @@ function nextSentence() {
 }
 
 function prev() {
-  if (player.sound.playing()) {
-    player.sound.pause();
+  if (player.sound.value.playing()) {
+    player.sound.value.pause();
   }
 
   if (lessons.value.indexOf(player.currentLesson) > 0) {
@@ -348,8 +299,8 @@ function prev() {
 }
 
 function next() {
-  if (player.sound.playing()) {
-    player.sound.pause();
+  if (player.sound.value.playing()) {
+    player.sound.value.pause();
   }
 
   if (lessons.value.indexOf(player.currentLesson) < lessons.value.length - 1) {
